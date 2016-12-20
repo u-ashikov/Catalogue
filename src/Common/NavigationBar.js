6d48dedcaf031d94088c786d098d4a96df886a14
u-ashikov <u.ashikov@abv.bg>
2016-12-20 16:18:58 +0200

    Some changes

--- src/Common/NavigationBar.js
+++ src/Common/NavigationBar.js
@@ -11,7 +11,7 @@ export default class NavigationBar extends Component {
                         <ul className="nav navbar-nav">
                             <li className="active"><a href="#">Home</a></li>
                             <li><a href="#">Page 1</a></li>
-                            <li><a href="#">Page 1</a></li>
+                            <li><a href="#">Page 2</a></li>
                         </ul>
                         <ul className="nav navbar-nav navbar-right">
                             <li><a href="#"><span className="glyphicon glyphicon-user"></span> Register</a></li>

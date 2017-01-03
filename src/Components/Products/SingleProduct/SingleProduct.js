97288e0163e231a8853086fd25214995d4d28eb7
u-ashikov <u.ashikov@abv.bg>
2017-01-03 17:57:38 +0200

    Handle on product picture click redirect to its details

--- src/Components/Products/SingleProduct/SingleProduct.js
+++ src/Components/Products/SingleProduct/SingleProduct.js
@@ -5,12 +5,12 @@ export default class SingleProduct extends Component {
     render() {
         return (
             <figure>
-                <img src={this.props.picture} alt="product" width='190' height='280'/>
+                <img className="product-pic" src={this.props.picture} alt="product" width='190' height='280' onClick={this.props.onClickHandler}/>
                 <figcaption>{this.props.title}</figcaption>
                     <span className="price">
                         {this.props.price}
                     </span>
-                    <a className="button" onClick={this.props.onClickHandler}>Buy Now</a>
+                    <a className="button" onClick={this.props.onClickHandler}>More Info</a>
             </figure>
         )
     }

81e842f9dd397fa311fe9b9e2385a7f24e364085
u-ashikov <u.ashikov@abv.bg>
2017-01-09 13:47:11 +0200

    Delivery info form submit handling

--- src/Components/Order/OrderInfo.js
+++ src/Components/Order/OrderInfo.js
@@ -4,29 +4,54 @@ import '../../styles/order-info.css';
 export default class OrderInfo extends Component {
     render() {
         return (
-            <form className="cf">
+            <form className="cf" onSubmit={this.props.onSubmitHandler}>
                 <div className="header">
                     Delivery info
                 </div>
                 <div className="half left cf">
                     <input
+                        name="fullName"
                         id="input-name"
                         type="text"
-                        placeholder="Full name"
+                        value={this.props.fullName}
+                        onChange={this.props.onChangeHandler}
+                        placeholder="Full name*"
+                        required
                     />
                     <input
+                        name="email"
                         id="input-email"
                         type="email"
-                        placeholder="Email"
+                        value={this.props.email}
+                        onChange={this.props.onChangeHandler}
+                        placeholder="Email*"
+                        required
                     />
                     <input
+                        name="phone"
                         id="input-phone"
                         type="text"
-                        placeholder="Phone number"
+                        value={this.props.phone}
+                        onChange={this.props.onChangeHandler}
+                        placeholder="Phone number*"
+                        required
                     />
+                    <br/>
+                    <span className="require-info">
+                        The fields market with * are required!
+                    </span>
                 </div>
                 <div className="half right cf">
-                    <textarea name="message" type="text" id="input-message" placeholder="Delivery address"></textarea>
+                    <textarea
+                        name="address"
+                        type="text"
+                        id="input-message"
+                        value={this.props.address}
+                        onChange={this.props.onChangeHandler}
+                        placeholder="Delivery address*"
+                        required
+                    >
+                    </textarea>
                 </div>
                 <input type="submit" value="CONFIRM ORDER" id="input-submit"/>
             </form>
--- src/Components/Order/OrderInfoController.js
+++ src/Components/Order/OrderInfoController.js
@@ -2,9 +2,36 @@ import React, {Component} from 'react';
 import OrderInfo from '../Order/OrderInfo';
 
 export default class OrderInfoController extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            fullName:'',
+            email:'',
+            phone:'',
+            address:''
+        }
+    }
     render() {
         return (
-            <OrderInfo/>
+            <OrderInfo
+                fullName={this.state.fullName}
+                email={this.state.email}
+                phone={this.state.phone}
+                address={this.state.address}
+                onChangeHandler={this.onChangeHandler.bind(this)}
+                onSubmitHandler={this.submitOrder.bind(this)}
+            />
         )
+    }
+
+    onChangeHandler(event) {
+        let newState={};
+        newState[event.target.name]=event.target.value;
+        this.setState(newState);
+    }
+
+    submitOrder(event) {
+        event.preventDefault();
+        console.dir(this.state);
     }
 }

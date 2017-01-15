cb3988029ab4c16bd1213ee4f0541031a0b0e238
u-ashikov <u.ashikov@abv.bg>
2017-01-15 18:40:36 +0200

    Orders table

--- src/Components/User/Admin/Orders/ReceivedOrder.js
+++ src/Components/User/Admin/Orders/ReceivedOrder.js
@@ -3,7 +3,42 @@ import React, {Component} from 'react';
 export default class ReceivedOrder extends Component {
     render() {
         return (
-            <div>Hello</div>
+            <div className="container">
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Category</th>
+                            <th>Name</th>
+                            <th>Code</th>
+                            <th>Order Count</th>
+                            <th>Price</th>
+                            <th>Total</th>
+                            <th>Customer</th>
+                            <th>Address</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {Object.keys(this.props.orders).map(key=>this.props.orders[key].map(singleOrder=><tr key={singleOrder._id}>
+                        <td>Image</td>
+                        <td>{singleOrder.category}</td>
+                        <td>{singleOrder.name}</td>
+                        <td>{singleOrder.code}</td>
+                        <td>{singleOrder.orderCount}</td>
+                        <td>{Number(singleOrder.price).toFixed(2)}</td>
+                        <td>{Number(singleOrder.totalPrice).toFixed(2)}</td>
+                        <td>{singleOrder.customerName}</td>
+                        <td>{singleOrder.deliveryAddress}</td>
+                        <td>{singleOrder.customerEmail}</td>
+                        <td>{singleOrder.customerPhone}</td>
+                        <td><button>Process</button></td>
+                    </tr>))}
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
--- src/Components/User/Admin/Orders/ReceivedOrderController.js
+++ src/Components/User/Admin/Orders/ReceivedOrderController.js
@@ -5,18 +5,34 @@ import OrderModel from '../../../../Models/OrderModel';
 export default class ReceivedOrderController extends Component {
     constructor(props) {
         super(props);
+        this.state={
+            'orders':{}
+        }
     }
 
     componentDidMount() {
+        let _self=this;
         OrderModel.getAllOrders()
-            .then(function (response) {
-                console.dir(response);
+            .then(function (orders) {
+                let ordersByCustomers={};
+                for (let order of orders) {
+                    if (!Object.keys(ordersByCustomers).includes(order.customerId)) {
+                        ordersByCustomers[order.customerId]=[];
+                    }
+                    ordersByCustomers[order.customerId].push(order);
+                }
+                _self.setState({
+                    'orders':ordersByCustomers
+                });
+                console.dir(_self.state);
             });
     }
 
     render() {
         return (
-            <ReceivedOrder/>
+            <ReceivedOrder
+                orders={this.state.orders}
+            />
         )
     }
 }

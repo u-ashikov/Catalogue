23961567077dafcfdbc13527bb134902312d8c04
u-ashikov <u.ashikov@abv.bg>
2017-01-15 12:29:24 +0200

    Admin panel prototype

--- src/Components/Common/NavigationBar.js
+++ src/Components/Common/NavigationBar.js
@@ -9,6 +9,53 @@ export default class NavigationBar extends Component {
         }
     }
     render() {
+        if (sessionStorage.getItem('authToken') && sessionStorage.getItem('userId')==='5877a8ee350958684b335a93') {
+            return (
+                <nav className="navbar navbar-inverse">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <Link className="navbar-brand" to="/home">Catalog</Link>
+                        </div>
+                        <div>
+                            <ul className="nav navbar-nav">
+                                <li><Link to="/necklaces">Necklaces</Link></li>
+                                <li><Link to="/bracelets">Bracelets</Link></li>
+                                <li><Link to="/rings">Rings</Link></li>
+                                <li><Link to="/earrings">Earrings</Link></li>
+                            </ul>
+                            <ul className="nav navbar-nav navbar-right">
+                                <li className="dropdown"
+                                    onClick={this.handleToggle.bind(this)}
+                                >
+                                    <Link
+                                        className="dropdown-toggle"
+                                        style={{
+                                            cursor:'pointer'
+                                        }}
+                                    >Profile<span className="glyphicon glyphicon-user"></span> <span className="caret"></span></Link>
+                                    <ul
+                                        className="dropdown-menu"
+                                        style={{display: this.state.showItems ? 'block' : 'none' }}
+                                    >
+                                        <li
+                                            onClick={this.handleToggle.bind(this)}
+                                        ><Link
+                                            to="/my-orders"
+                                        >My Orders</Link>
+                                        </li>
+                                        <li
+                                            onClick={this.handleToggle.bind(this)}
+                                        ><Link
+                                            to="/logout"
+                                        >Logout</Link></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            )
+        }
         return (
             <nav className="navbar navbar-inverse">
                 <div className="container-fluid">

2c556a5e1feab894c89072cacb7e72031a1fbd3c
u-ashikov <u.ashikov@abv.bg>
2017-01-16 11:45:45 +0200

    Customer orders view

--- src/Components/User/Admin/Orders/ReceivedOrder.js
+++ src/Components/User/Admin/Orders/ReceivedOrder.js
@@ -5,40 +5,46 @@ export default class ReceivedOrder extends Component {
     render() {
         return (
             <div id="orders-view" className="container">
-                <table id="customer-orders" className="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>Image</th>
-                            <th>Category</th>
-                            <th>Name</th>
-                            <th>Code</th>
-                            <th>Order Count</th>
-                            <th>Price</th>
-                            <th>Total</th>
-                            <th>Customer</th>
-                            <th>Address</th>
-                            <th>Email</th>
-                            <th>Phone</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {Object.keys(this.props.orders).map(key=>this.props.orders[key].map(singleOrder=><tr key={singleOrder._id}>
-                        <td>Image</td>
-                        <td>{singleOrder.category}</td>
-                        <td>{singleOrder.name}</td>
-                        <td>{singleOrder.code}</td>
-                        <td>{singleOrder.orderCount}</td>
-                        <td>{Number(singleOrder.price).toFixed(2)}</td>
-                        <td>{Number(singleOrder.totalPrice).toFixed(2)}</td>
-                        <td>{singleOrder.customerName}</td>
-                        <td>{singleOrder.deliveryAddress}</td>
-                        <td>{singleOrder.customerEmail}</td>
-                        <td>{singleOrder.customerPhone}</td>
-                        <td><button>Process</button></td>
-                    </tr>))}
-                    </tbody>
-                </table>
+                    {Object.keys(this.props.orders).map(key=>
+                    <div>
+                        <table id="customer-orders" className="table table-hover">
+                            <thead>
+                            <tr>
+                                <th>Image</th>
+                                <th>Category</th>
+                                <th>Name</th>
+                                <th>Code</th>
+                                <th>Order Count</th>
+                                <th>Price</th>
+                                <th>Total</th>
+                                <th>Customer</th>
+                                <th>Address</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {this.props.orders[key].map(order=>
+                            <tr key={order._id}>
+                                <td>Image</td>
+                                <td>{order.category}</td>
+                                <td>{order.name}</td>
+                                <td>{order.code}</td>
+                                <td>{order.orderCount}</td>
+                                <td>{order.price}</td>
+                                <td>{order.totalPrice}</td>
+                                <td>{order.customerName}</td>
+                                <td>{order.deliveryAddress}</td>
+                                <td>{order.customerEmail}</td>
+                                <td>{order.customerPhone}</td>
+                                <td><button>Process</button></td>
+                            </tr>
+                            )}
+                            </tbody>
+                        </table>
+                    </div>
+                    )}
             </div>
         )
     }

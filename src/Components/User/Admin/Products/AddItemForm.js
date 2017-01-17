f276088060127f40a746c448ea7034a63ac29145
u-ashikov <u.ashikov@abv.bg>
2017-01-17 18:32:46 +0200

    Add item form prototype

--- src/Components/User/Admin/Products/AddItemForm.js
+++ src/Components/User/Admin/Products/AddItemForm.js
@@ -4,8 +4,42 @@ export default class AddItemForm extends Component {
     render() {
         return (
             <form>
-                <label>Name</label>
-                <input className="form-control"/>
+                <div>
+                    <label>Name: </label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>Description: </label>
+                    <textarea type="text"/>
+                </div>
+                <div>
+                    <label>Code: </label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>Size: </label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>Quantity: </label>
+                    <input type="number"/>
+                </div>
+                <div>
+                    <label>Price: </label>
+                    <input type="number"/>
+                </div>
+                <div>
+                    <label>Category: </label>
+                    <select>
+                        <option>Necklaces</option>
+                        <option>Rings</option>
+                        <option>Bracelets</option>
+                        <option>Earrings</option>
+                    </select>
+                </div>
+                <div>
+                    <button type="submit">Add Item</button>
+                </div>
             </form>
         )
     }

b80e84fcc383d1615639c47186c844a363942679
u-ashikov <u.ashikov@abv.bg>
2017-01-17 21:56:20 +0200

    Form view, css

--- src/Components/User/Admin/Products/AddItemForm.js
+++ src/Components/User/Admin/Products/AddItemForm.js
@@ -1,46 +1,51 @@
 import React, {Component} from 'react';
+import '../../../../styles/add-item-form.css';
 
 export default class AddItemForm extends Component {
     render() {
         return (
-            <form>
-                <div>
-                    <label>Name: </label>
-                    <input type="text"/>
-                </div>
-                <div>
-                    <label>Description: </label>
-                    <textarea type="text"/>
-                </div>
-                <div>
-                    <label>Code: </label>
-                    <input type="text"/>
-                </div>
-                <div>
-                    <label>Size: </label>
-                    <input type="text"/>
-                </div>
-                <div>
-                    <label>Quantity: </label>
-                    <input type="number"/>
-                </div>
-                <div>
-                    <label>Price: </label>
-                    <input type="number"/>
-                </div>
-                <div>
-                    <label>Category: </label>
-                    <select>
-                        <option>Necklaces</option>
-                        <option>Rings</option>
-                        <option>Bracelets</option>
-                        <option>Earrings</option>
-                    </select>
-                </div>
-                <div>
-                    <button type="submit">Add Item</button>
-                </div>
-            </form>
+            <div id="form-holder" className="container">
+                <form className="add-item">
+                    <h1 className="heading">Add item form</h1>
+                    <hr/>
+                    <div className="form-group">
+                        <label>Name: </label>
+                        <input className="form-control" type="text"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <textarea className="form-control" type="text"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Code: </label>
+                        <input className="form-control" type="text"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Size: </label>
+                        <input className="form-control" type="text"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Quantity: </label>
+                        <input className="form-control" type="number"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Price: </label>
+                        <input className="form-control" type="number"/>
+                    </div>
+                    <div>
+                        <label>Category: </label>
+                        <select className="form-control">
+                            <option>Necklaces</option>
+                            <option>Rings</option>
+                            <option>Bracelets</option>
+                            <option>Earrings</option>
+                        </select>
+                    </div>
+                    <div>
+                        <button className="btn btn-success" type="submit">Add Item</button>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
